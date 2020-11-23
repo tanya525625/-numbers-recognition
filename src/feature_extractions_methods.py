7b6bca903a2c115cee714bdc5fa4a85df4730acb
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2020-11-23 16:57:14 +0300

    Added haar method for features' extraction

--- src/feature_extractions_methods.py
+++ src/feature_extractions_methods.py
@@ -12,3 +12,13 @@ def make_mean_value_in_square(image, window=10):
             square = image[i:i + window, j:j + window]
             squares_mean_values.append(np.mean(square))
     return squares_mean_values
+
+
+def haar_features(image, window=10):
+    image = utils.resize_image(image, window)
+    h, w = image.shape
+    half = int(w / 2)
+    transposed_img = np.transpose(image)
+    left_part = transposed_img[:half]
+    right_prt = transposed_img[half:]
+    return [np.mean(left_part), np.mean(right_prt)]
--- src/recognition.py
+++ src/recognition.py
@@ -8,7 +8,7 @@ def main():
     data_dir = Path('../data')
     img_path = data_dir / 'test_5.jpg'
 
-    methods = [fem.make_mean_value_in_square]
+    methods = [fem.make_mean_value_in_square, fem.haar_features]
     recognizer = Recognizer(methods)
     recognizer.recognize(img_path)
 
